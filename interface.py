97e6afc9c9ba9ff3e7e7a09b1f6d570a4af6d470
Carlos Salazar <carlerafael@hotmail.com>
2022-06-24 17:38:20 +0200

    Removed sorting and averaging as it is no longer necessary

--- interface.py
+++ interface.py
@@ -4,27 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpi4py import MPI
 
-def sorting(filename):
-    f1 = open(filename)
-    infile = f1.readlines()
-    startAtoms = 0
-    numAtoms = 0
-    for i in range(len(infile)):
-        if 'atom types' in infile[i]:
-            numAtoms = int(infile[i-1].split()[0])
-        if 'Atoms # atomic' in infile[i]:
-            startAtoms = i+2
-    output = sorted(infile[startAtoms:startAtoms+numAtoms], key=lambda x: int(x.split()[0]))
-    outfile = open("sorted.data", "w")
-    outfile.writelines(infile[:startAtoms] + output)
-    outfile.close()
-
-ave = []
-errAve = []
-
 # Python script to perform a simulation with nnp potentials and check agreement
 potentialSeed = 1
 dataDir = 'PhysicalPropertiesTemplate/Configurations/Au100Messy.data'
+disagreement = []
 
 totalSteps = 1000
 checkEvery = 100
@@ -45,49 +28,29 @@ lmp.file('sim.melt')
 # me = MPI.COMM_WORLD.Get_rank()
 # nprocs = MPI.COMM_WORLD.Get_size()
 # print("Proc %d out of %d procs has" % (me,nprocs),lmp)
-#MPI.Finalize()
 
 for a in range(checkSteps):
     lmp.command('run '+str(checkEvery))
 
     lmp.command('write_data check.data')
-    sorting('check.data')
 
-    devs = []
+    seeds = [None]*5
+    forces = [None]*5
+    for b in range(5):
+        seeds[b] = lammps.lammps()
+        seeds[b].command('units metal')
+        seeds[b].command('boundary p p p')
+        seeds[b].command('read_data check.data')
 
-    for i in range(10):
-        seeds = [None]*5
-        forces = [None]*5
-        for b in range(5):
-            seeds[b] = lammps.lammps()
-            seeds[b].command('units metal')
-            seeds[b].command('boundary p p p')
-            seeds[b].command('read_data sorted.data')
+        seeds[b].command('pair_style nnp dir PotentialsComplete/Seed'+str(b+1)+' showew no showewsum 100 resetew no maxew 10000000 cflength 1.0 cfenergy 1.0 emap "2:Au"')
+        seeds[b].command('pair_coeff * * 6.01')
+        seeds[b].command('run 0')
+        forces[b] = seeds[b].numpy.extract_atom('f')
+    deviationAtom = np.std(np.ma.masked_invalid(forces), axis=0)
+    deviationMax = np.ma.masked_invalid(deviationAtom).max()
+    disagreement.append(deviationMax)
 
-            seeds[b].command('pair_style nnp dir PotentialsComplete/Seed'+str(b+1)+' showew no showewsum 100 resetew no maxew 10000000 cflength 1.0 cfenergy 1.0 emap "2:Au"')
-            seeds[b].command('pair_coeff * * 6.01')
-            seeds[b].command('run 0')
-            forces[b] = seeds[b].numpy.extract_atom('f')
-        #print(forces)
-        deviationAtom = np.std(np.ma.masked_invalid(forces), axis=0)
-        #print(deviationAtom)
-        masked = np.ma.masked_greater(np.ma.masked_invalid(deviationAtom), 1000)
-        deviationMax = masked.max()
-        devs.append(deviationMax)
-        print(deviationMax)
-        # for s in range(len(deviationAtom)):
-        #     if np.isnan(deviationAtom[s]).any() or np.isinf(deviationAtom[s]).any():
-        #         print(s)
-        #         for l in range(5):
-        #             print(forces[l][s])
-
-    ave.append(np.mean(devs))
-    errAve.append(np.std(devs))
-    # ave[a].append(np.ma.masked_invalid(devs).mean())
-    # errAve[a].append(np.ma.masked_invalid(devs).std())
-
-ave = np.array(ave)
-errAve = np.array(errAve)
+disagreement = np.array(disagreement)
 
 plt.rcParams["axes.prop_cycle"] = plt.cycler("color", plt.cm.Dark2.colors)
 fig, ax1 = plt.subplots()
@@ -96,10 +59,9 @@ ax1.set_title('Evolution (100) Au (T = 1000)')
 ax1.set_ylabel('Disagreement')
 ax1.set_xlabel('Timestep')
 
-x = (np.arange(len(ave))+1)*checkEvery
+x = (np.arange(len(disagreement))+1)*checkEvery
 
-ax1.errorbar(x, ave, yerr=errAve, label='Disagreement', fmt=':', alpha=0.75, capsize=5.0, color='C0')
-ax1.fill_between(x, ave-errAve, ave+errAve, alpha=0.25, color='C0')
+ax1.errorbar(x, disagreement, label='Disagreement', color='C0')
 
 ax1.legend()
 plt.savefig('timeAgreement.png')
--- interfaceTransferability.py
+++ interfaceTransferability.py
@@ -4,23 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpi4py import MPI
 
-def sorting(filename):
-    f1 = open(filename)
-    infile = f1.readlines()
-    startAtoms = 0
-    numAtoms = 0
-    for i in range(len(infile)):
-        if 'atom types' in infile[i]:
-            numAtoms = int(infile[i-1].split()[0])
-        if 'Atoms # atomic' in infile[i]:
-            startAtoms = i+2
-    output = sorted(infile[startAtoms:startAtoms+numAtoms], key=lambda x: int(x.split()[0]))
-    outfile = open("sorted.data", "w")
-    outfile.writelines(infile[:startAtoms] + output)
-    outfile.close()
-
-ave = []
-errAve = []
+disagreement = []
 
 # Python script to perform a simulation with nnp potentials and check agreement
 potentialSeed = 1
@@ -38,54 +22,34 @@ lmp.command('pair_style nnp dir '+locationSeeds[0]+'/Seed'+str(potentialSeed)+' 
 lmp.command('pair_coeff * * 6.01')
 
 lmp.file('sim.melt')
-lmp.command('run 10')
+lmp.command('run 0')
 
 #lmp.command('run 0')
 # me = MPI.COMM_WORLD.Get_rank()
 # nprocs = MPI.COMM_WORLD.Get_size()
 # MPI.Finalize()
 
-#lmp.command('write_dump              all custom check.lammpstrj id type x y z fx fy fz modify sort id')
 lmp.command('write_data check.data')
-sorting('check.data')
-# trj = read_lammps_data('check.data', sort_by_id=True, style='atomic')
-# write_lammps_data('check copy.data', trj)
-# print(trj.get_chemical_symbols())
 
 for a in range(len(locationSeeds)):
-    ave.append([])
-    errAve.append([])
-    devs = []
+    disagreement.append([])
 
-    for i in range(10):
-        seeds = [None]*5
-        forces = [None]*5
-        for b in range(len(seeds)):
-            seeds[b] = lammps.lammps()
-            seeds[b].command('units metal')
-            seeds[b].command('boundary p p p')
-            seeds[b].command('read_data sorted.data')
+    seeds = [None]*5
+    forces = [None]*5
+    for b in range(len(seeds)):
+        seeds[b] = lammps.lammps()
+        seeds[b].command('units metal')
+        seeds[b].command('boundary p p p')
+        seeds[b].command('read_data check.data')
 
-            seeds[b].command('pair_style nnp dir '+locationSeeds[a]+'/Seed'+str(b+1)+' showew no showewsum 100 resetew no maxew 10000000 cflength 1.0 cfenergy 1.0 emap "2:Au"')
-            seeds[b].command('pair_coeff * * 6.01')
-            seeds[b].command('run 0')
-            forces[b] = seeds[b].numpy.extract_atom('f')
-        #print(forces)
-        deviationAtom = np.std(np.ma.masked_invalid(forces), axis=0)
-        #print(deviationAtom)
-        deviationMax = np.ma.masked_invalid(deviationAtom).std()
-        devs.append(deviationMax)
-        print(deviationMax)
-        # for s in range(len(deviationAtom)):
-        #     if np.isnan(deviationAtom[s]).any() or np.isinf(deviationAtom[s]).any():
-        #         print(s)
-        #         for l in range(5):
-        #             print(forces[l][s])
+        seeds[b].command('pair_style nnp dir '+locationSeeds[a]+'/Seed'+str(b+1)+' showew no showewsum 100 resetew no maxew 10000000 cflength 1.0 cfenergy 1.0 emap "2:Au"')
+        seeds[b].command('pair_coeff * * 6.01')
+        seeds[b].command('run 0')
+        forces[b] = seeds[b].numpy.extract_atom('f')
+    deviationAtom = np.std(np.ma.masked_invalid(forces), axis=0)
+    deviationMax = np.ma.masked_invalid(deviationAtom).max()
 
-    ave[a].append(np.mean(devs))
-    errAve[a].append(np.std(devs))
-    # ave[a].append(np.ma.masked_invalid(devs).mean())
-    # errAve[a].append(np.ma.masked_invalid(devs).std())
+    disagreement[a].append(deviationMax)
 
 plt.rcParams["axes.prop_cycle"] = plt.cycler("color", plt.cm.Dark2.colors)
 plt.rcParams["xtick.minor.visible"] = False
@@ -109,17 +73,16 @@ plt.xlabel('Surface')
 
 # Make the plot
 #plt.bar(r1, dft, width=barWidth, label='DFT value')
-plt.bar(r1, ave[0], yerr=errAve[0], ecolor='black', capsize=10, width=barWidth, label='Complete Training')
-plt.bar(r2, ave[1], yerr=errAve[1], ecolor='black', capsize=10, width=barWidth, label='No 100 Training')
-plt.bar(r3, ave[2], yerr=errAve[2], ecolor='black', capsize=10, width=barWidth, label='No 111 Training')
-plt.bar(r4, ave[3], yerr=errAve[3], ecolor='black', capsize=10, width=barWidth, label='No Surface Training')
+plt.bar(r1, disagreement[0], ecolor='black', width=barWidth, label='Complete Training')
+plt.bar(r2, disagreement[1], ecolor='black', width=barWidth, label='No 100 Training')
+plt.bar(r3, disagreement[2], ecolor='black', width=barWidth, label='No 111 Training')
+plt.bar(r4, disagreement[3], ecolor='black', width=barWidth, label='No Surface Training')
  
 # Add xticks on the middle of the group bars
 plt.xticks([r + barWidth for r in range(1)], surfaces)
 
- 
 # Create legend & Show graphic
-#plt.yscale('log')
+plt.yscale('log')
 plt.grid(axis='y', which='major')
 plt.grid(axis='y', which='minor', linewidth=0.5)
 plt.legend()
