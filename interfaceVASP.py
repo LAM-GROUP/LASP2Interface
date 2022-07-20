df15f15be01c098a7dee867157a1d92f97d20cdc
carle13 <carlerafael@hotmail.com>
2022-07-20 14:59:43 +0200

    Fix VASP number of atoms

--- interfaceVASP.py
+++ interfaceVASP.py
@@ -13,7 +13,7 @@ def compute(training):
     computeDir = os.path.join(dftDir, 'dft'+str(training))
     os.system('cp -r vaspInput '+computeDir)
     lammps = lammpsdata.read_lammps_data('check.data',  style='atomic')
-    lammps.symbols = 'Au144'
+    lammps.symbols = 'Au'+str(len(lammps))
     os.chdir(computeDir)
     vasp.write_vasp('POSCAR', lammps)
     os.system('time srun $(placement ${SLURM_NTASKS_PER_NODE} 1 ) $binaire  > output_${SLURM_JOBID}')
