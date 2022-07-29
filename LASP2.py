fefc6f7cee306c25f912e8e41196fd4dbb6bc0df
carle13 <carlerafael@hotmail.com>
2022-07-29 17:46:08 +0200

    fix exit error

--- LASP2.py
+++ LASP2.py
@@ -3,6 +3,7 @@ import sys
 from sys import exit
 import subprocess
 from subprocess import Popen
+import re
 import configparser
 import ase
 import time
@@ -107,9 +108,9 @@ trainings = 1
 lammpsRun = Popen('time srun $(placement ${SLURM_NTASKS_PER_NODE} 1 ) python3 /tmpdir/fresseco/install/LASP2Interface/interfaceLAMMPS.py --start -config '+inputFile+' -iteration '+str(trainings)+' > lasp2_'+str(trainings)+'.out', shell=True, stderr=subprocess.PIPE)
 exitErr = lammpsRun.stderr.read().decode()
 print('LAMMPS exited with stderr: '+exitErr)
-print(exitErr == '50')
+print(re.match('^50', exitErr))
 while True:
-    if exitErr == '50': #Exit code returned when the flag for training is activated
+    if re.match('^50', exitErr): #Exit code returned when the flag for training is activated
         print('Performing DFT calculations         Iteration: '+str(trainings))
         compute(trainings)
         print('Performing NNP training             Iteration: '+str(trainings))
